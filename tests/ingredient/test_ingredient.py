c51052ac67a6e28719d94d754feb985fed915e19
Claydson Henrique <claydsonhenriquebs@gmail.com>
2024-02-20 18:27:03 -0300

    dcorrigindo requisito 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,56 +1,54 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+# import pytest
+from src.models.ingredient import Ingredient
 
 
 def test_instantiate_ingredient_with_valid_arguments():
-    ingredient = Ingredient("Tomato", ["vegetarian", "gluten-free"])
+    ingredient = Ingredient("Tomato")
     assert isinstance(ingredient, Ingredient)
 
 
 def test_check_ingredient_name():
     name = "Tomato"
-    ingredient = Ingredient(name, [])
+    ingredient = Ingredient(name)
     assert ingredient.name == name
 
 
 def test_verify_ingredient_restrictions():
-    restrictions = ["vegetarian", "gluten-free"]
-    ingredient = Ingredient("Tomato", restrictions)
-    assert ingredient.restrictions == set(restrictions)
+    restrictions = {"vegetarian", "gluten-free"}
+    ingredient = Ingredient("Tomato")
+    assert ingredient.restrictions == restrictions
 
 
 def test_representation_of_ingredient():
-    ingredient = Ingredient("Tomato", ["vegetarian", "gluten-free"])
-    expected_repr = (
-        "Ingredient(name='Tomato', "
-        "restrictions=['vegetarian', 'gluten-free'])"
-    )
+    ingredient = Ingredient("Tomato")
+    expected_repr = "Ingredient(name='Tomato')"
     assert repr(ingredient) == expected_repr
 
 
 def test_compare_two_equal_ingredients():
-    ingredient1 = Ingredient("Tomato", ["vegetarian", "gluten-free"])
-    ingredient2 = Ingredient("Tomato", ["vegetarian", "gluten-free"])
+    ingredient1 = Ingredient("Tomato")
+    ingredient2 = Ingredient("Tomato")
     assert ingredient1 == ingredient2
 
 
 def test_verify_hash_of_ingredient():
-    ingredient1 = Ingredient("Tomato", ["vegetarian", "gluten-free"])
-    ingredient2 = Ingredient("Tomato", ["vegetarian", "gluten-free"])
+    ingredient1 = Ingredient("Tomato")
+    ingredient2 = Ingredient("Tomato")
     assert hash(ingredient1) == hash(ingredient2)
 
 
 def test_compare_hash_of_different_ingredients():
-    ingredient1 = Ingredient("Tomato", ["vegetarian", "gluten-free"])
-    ingredient2 = Ingredient("Onion", ["vegetarian", "gluten-free"])
+    ingredient1 = Ingredient("Tomato")
+    ingredient2 = Ingredient("Onion")
     assert hash(ingredient1) != hash(ingredient2)
 
 
 def test_compare_different_ingredients():
-    ingredient1 = Ingredient("Tomato", ["vegetarian", "gluten-free"])
-    ingredient2 = Ingredient("Onion", ["vegetarian", "gluten-free"])
+    ingredient1 = Ingredient("Tomato")
+    ingredient2 = Ingredient("Onion")
     assert ingredient1 != ingredient2
 
 
 def test_representation_of_ingredient_incorrect():
-    ingredient = Ingredient("Tomato", ["vegetarian", "gluten-free"])
+    ingredient = Ingredient("Tomato")
     assert repr(ingredient) != "Ingredient(name='Tomato')"
